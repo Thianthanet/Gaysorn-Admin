fe5ad3bf68593f098c9a6404aa02c21a8011b5a2
Thianthanet <EXRAZER@icloud.com>
2025-07-22 16:15:10 +0700

    UpdateCode

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// // import tailwindcss from '@tailwindcss/vite'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+    },
+  },
 })
