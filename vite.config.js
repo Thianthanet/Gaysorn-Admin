b7691c46e7e3ecc97c6a77de5b16c6e6afcb233e
Thianthanet <EXRAZER@icloud.com>
2025-07-22 16:24:16 +0700

    UpdateCode

--- vite.config.js
+++ vite.config.js
@@ -1,46 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// // import tailwindcss from '@tailwindcss/vite'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+// import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  resolve: {
-    alias: {
-      crypto: 'crypto-browserify',
-      stream: 'stream-browserify',
-    },
-  },
-  define: {
-    global: 'globalThis',
-    'process.env': {},
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [],
-    },
-  },
-  build: {
-    rollupOptions: {
-      plugins: [
-        rollupNodePolyFill(),
-      ],
-    },
-  },
+  plugins: [react()],
 })
-
-
